6d683f00968aae7e61c4d709299b86304fa423f2
sadaharusong <jacksomangel@qq.com>
2016-04-05 20:51:04 +0800

    reflect
    Class类的使用，实现动态加载类，获取方法信息，获取成员变量构造函数信息，方法反射的操作，以及通过反射了解集合泛型的本质。

---
+++ src/com/sadaharu/ClassDemo3.java
@@ -0,0 +1,12 @@
+package com.sadaharu;
+
+public class ClassDemo3 {
+	public static void main(String[] args) {
+		String s = "hello";
+		ClassUtil.printClassMethodMessage(s);
+		
+	    Integer n1 = 1;
+	    ClassUtil.printClassMethodMessage(n1);
+	}
+
+}
